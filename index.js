5094545c1e1c40a20a59fe8a2f17eb761e737f09
DmitryChernous <dmitry.chernous@yandex.ru>
2022-10-28 16:11:04 +0300

    второй коммит

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const TelegramApi = require('node-telegram-bot-api')
 const token = process.env.TOKEN
 const bot = new TelegramApi(token, {polling: true})
 
+
 bot.on('message', async msg => {
     const text = msg.text
     const chatId = msg.chat.id
